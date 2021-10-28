1fd9a547237c1804a81f3f6496f8d8f89bba5f41
Chris Rainbow <chris.s.rainbow@gmail.com>
2021-10-27 23:36:07 -0400

    Built Form functionality, and  calls API on submit

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,26 @@
 // JS file to manipulate the DOM
 
-const GetLocation = document.querySelector("form")
+const cityForm = document.querySelector("form")
 
-console.log("form")
+
+
+const updateCity = async (city) => {
+const cityKey = await getCity(city);
+const CityWeather = await getWeather(cityKey.Key);
+
+return {cityDetails: cityKey,
+        weather: CityWeather
+}
+}
+
+cityForm.addEventListener("submit", e =>{
+    e.preventDefault();
+    const city =  cityForm.city.value.trim();
+    cityForm.reset();
+
+    updateCity(city).then((data, err ) =>{
+            console.log(data)}) 
+        .catch(err => {
+            console.log(err, "there was an error")
+        });
+});
